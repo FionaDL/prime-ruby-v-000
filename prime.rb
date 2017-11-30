847c4503ebb6675aa05e085084dced4a1a51bf6b
Fiona Lapham <fionadlapham@gmail.com>
2017-11-30 19:50:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime? (n)
+ return false if n < 2 || n%2 == 0
+ array = *(2..n)
+array.each do |number|
+  new_n = n-1/number
+  counter = 0
+  while new_n%2 != 0
+    false
+   counter += 1
+  
+  end
+end
